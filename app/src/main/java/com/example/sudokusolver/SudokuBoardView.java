9455cf7c34318b1c1106931fa419aa50d6a3e5c7
Rafal Banasiewicz <banasiewicz969696@gmail.com>
2020-05-08 23:47:18 +0200

    Grids drawing done

--- app/src/main/java/com/example/sudokusolver/SudokuBoardView.java
+++ app/src/main/java/com/example/sudokusolver/SudokuBoardView.java
@@ -4,23 +4,79 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Half;
 import android.view.View;
 
-import androidx.annotation.RequiresApi;
 
 
-public class SudokuBoardView {
+public class SudokuBoardView extends View{
 
-    private Paint thickLinePaint = new Paint() {
-        Style style = Style.STROKE;
-        int color = Color.BLACK;
-        float strokeWidth = 4F;
-    };
+    private int sqrtSize = 3;
+    private int size = 9;
 
-    void onCreate(Canvas canvas) {
-        canvas.drawRect(10,10,100,100, thickLinePaint);
+    private float cellSizePixels = 0;
+
+
+    Paint thickLinePaint = new Paint();
+    Paint thinLinePaint = new Paint();
+
+    public SudokuBoardView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
     }
+
+    private void setThickLinePaint(){
+        thickLinePaint.setColor(Color.BLACK);
+        thickLinePaint.setStyle(Paint.Style.STROKE);
+        thickLinePaint.setStrokeWidth(4F);
+    }
+
+    private void setThinLinePaint(){
+        thinLinePaint.setColor(Color.BLACK);
+        thinLinePaint.setStyle(Paint.Style.STROKE);
+        thinLinePaint.setStrokeWidth(2F);
+    }
+
+    public void onDraw(Canvas canvas) {
+        cellSizePixels = (float)this.getWidth() / size;
+        drawLines(canvas);
+    }
+
+    private void drawLines (Canvas canvas) {
+        setThickLinePaint();
+        setThinLinePaint();
+        canvas.drawRect(0F,0F,(float)this.getWidth(),(float)this.getHeight(),thickLinePaint);
+        Paint paintToUse;
+        for(int i = 1; i<=size ;i++) {
+            if( i % sqrtSize == 0) paintToUse = thickLinePaint;
+            else paintToUse = thinLinePaint;
+
+            canvas.drawLine(
+                    i * cellSizePixels,
+                    0F,
+                    i * cellSizePixels,
+                    (float)this.getHeight(),
+                    paintToUse
+            );
+
+            canvas.drawLine(
+                    0F,
+                    i * cellSizePixels,
+                    (float)this.getWidth(),
+                    i * cellSizePixels,
+                    paintToUse
+            );
+
+
+        }
+
+    }
+
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        int sizePixels = Math.min(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(sizePixels, sizePixels);
+    }
+
+
 }
